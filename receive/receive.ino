9a53bf771e85265c1ad9b02b15027f565cd642bf
Akshit Sood <akshitsoodpanchkula@gmail.com>
2021-12-12 17:21:00 +0530

    Cam and arduino working with qr identification

---
+++ receive/receive.ino
@@ -0,0 +1,41 @@
+String incomingByte ;    
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);   //left motors forward
+  //pinMode(3,OUTPUT);   //left motors reverse
+  pinMode(4,OUTPUT);   //right motors forward
+  //pinMode(5,OUTPUT);   //right motors reverse
+
+}
+void loop() {
+
+  if (Serial.available() > 0) {
+
+  incomingByte = Serial.readStringUntil('\n');
+
+    if (incomingByte == "left") {
+      Serial.write("Going Left");
+      digitalWrite(4,HIGH);
+      delay(2000);
+      digitalWrite(4,LOW);
+    }
+
+    else if (incomingByte == "right") {
+      Serial.write("Going Right");
+      digitalWrite(2,HIGH);
+      delay(2000);
+      digitalWrite(2,LOW);     
+    }
+
+    else{
+
+     digitalWrite(2,HIGH);
+     digitalWrite(4,HIGH);
+
+    }
+
+  }
+
+}
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    # for obj in decodedObjects:
+    #     print('Type : ', obj.type)
+    #     print('Data : ', obj.data.decode('utf-8'),'\n')  
